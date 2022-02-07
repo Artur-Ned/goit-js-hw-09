58d5e2d169dda53c75e99ee7c9fcd202dd97f746
NNASTYA2007 <advokat1707@gmail.com>
2022-02-07 23:55:29 +0200

    осталось добавить в 1 дз фунцию и стоп

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const bodyRef = document.querySelector('body');
+const btnStartRef = document.querySelector("[data-start]");
+const btnStoptRef = document.querySelector("[data-stop]");
+console.log(btnStoptRef);
+
+btnStartRef.addEventListener('click', onBtnStartclick)
+btnStoptRef.addEventListener('click', onBtnStopclick)
+
+function onBtnStartclick(e) {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+    btnStartRef.setAttribute("disabled", "disabled");
+    
+}
+
+function onBtnStopclick(e) {
+    // bodyRef.style.backgroundColor = getRandomHexColor();
+    btnStartRef.removeAttribute("disabled", "disabled");
+}
+
+// removeAttribute()
