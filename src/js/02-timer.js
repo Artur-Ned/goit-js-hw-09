a3ef6f498e850c796ed69f25974009f82e1c922f
NNASTYA2007 <advokat1707@gmail.com>
2022-02-09 23:22:30 +0200

    начал2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,4 +2,24 @@ import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 
 const inputRef = document.querySelector("#datetime-picker");
-console.log(inputRef);
+const buttonRef = document.querySelector("[data-start]");
+console.log(buttonRef);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(inputRef, options);
+
+inputRef.addEventListener("click", onClose);
+
+function onClose() { };
+
+const chooseTime = inputRef.value;
+console.log(chooseTime);
